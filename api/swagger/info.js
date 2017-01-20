d8d82c1e3dbd24fcf1a4c025e288fdbf4a901e9a
zacharyrote <zachary.rote@gmail.com>
2017-01-20 15:42:29 -0500

    initial commit

---
+++ api/swagger/info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+   info: {
+     title: "Message Delivery Network",
+     description: "This service is responsible for delivering messages to / from applications",
+     version: "1.0.0"
+   }
+ }
